85c1bd1b1b1cc7b5790e6b9fc2771070209876b5
yavaka <yavakasolutions@gmail.com>
2020-10-16 12:53:11 +0100

    App03: Student Grades - Add enrollStudent and isAlreadyEnrolled methods and refactored toString method

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The Course class represents a course in a student grades system.
  * It holds the course details a code number and title. 
@@ -10,16 +12,17 @@ public class Course {
     private String title;
     // the code number of this course
     private String codeNumber;
-
+    // enrolled students for this course
+    private ArrayList<Student> enrolledStudents;
+    
     /**
      * Create a new course with title and code number
-     * @param title
-     * @param codeNumber
      */
     public Course(String title, String codeNumber)
     {
        this.title = title;
        this.codeNumber = codeNumber;
+       this.enrolledStudents = new ArrayList<Student>();
     }
 
     /**
@@ -39,10 +42,53 @@ public class Course {
     }
 
     /**
+     * Enroll this student for new course
+     */
+    public void enrollStudent(Student newStudent)
+    {
+        if(isAlreadyEnrolled(newStudent)){
+            System.out.println("This student has already enrolled for " + this.title + " course!");
+        }
+        else{
+            this.enrolledStudents.add(newStudent);
+            System.out.println(newStudent.getName() + " has been enrolled for " + this.title + " course.");
+        }
+    }
+
+    /**
+     * Check is the new student already enrolled for this courses
+     */
+    private boolean isAlreadyEnrolled(Student newStudent) {
+        // iterate all enrolled students of this course
+        for (Student student : this.enrolledStudents) {
+
+            // if the new student details match with the current student destails return true 
+            if(student.getName() == newStudent.getName() &&
+                student.getStudentID() == newStudent.getStudentID()){
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    /**
      * Return text representation of this course details
      */
     @Override
     public String toString(){
-        return "Course: " + this.codeNumber + ", " + this.title;
+        StringBuilder sBuilder = new StringBuilder();
+        sBuilder.append(this.codeNumber + ", " + this.title + " course\n");
+
+        if(this.enrolledStudents.size() > 0){
+            sBuilder.append("Enrolled students:\n");
+            for (Student student : this.enrolledStudents) {
+                sBuilder.append("Id: "+ student.getStudentID() + " " + student.getName() + "\n");
+            }
+        }
+        else
+        {
+            sBuilder.append("There is no any enrolled students yet.\n");
+        }
+        return sBuilder.toString();
     }
 }
