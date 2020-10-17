3a80cd3d70f7b7e43dbb856b5c3ba6ba9e2b8374
yavaka <yavakasolutions@gmail.com>
2020-10-17 16:16:30 +0100

    App03: Student Grades - Add addModule, getModukes and isModuleExist methods in Course class.

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+
+import javax.lang.model.util.ElementScanner14;
+
 /**
  * The Course class represents a course in a student grades system. It holds the
  * course details a code number and title.
@@ -10,6 +14,8 @@ public class Course {
     private String title;
     // the code number of this course
     private String codeNumber;
+    // the modules of this course
+    private ArrayList<Module> modules;
 
     /**
      * Create a new course with title and code number
@@ -17,6 +23,7 @@ public class Course {
     public Course(String title, String codeNumber) {
         this.title = title;
         this.codeNumber = codeNumber;
+        this.modules = new ArrayList<Module>();
     }
 
     /**
@@ -31,6 +38,41 @@ public class Course {
      */
     public String getCodeNumber() {
         return this.codeNumber;
+    }
+
+    /**
+     * Return collection with all modules of this course
+     */
+    public ArrayList<Module> getModules() {
+        return this.modules;
+    }
+
+    /**
+     * Add new module to this course
+     */
+    public void addModule(Module newModule) {
+        if (this.modules.size() > 3) {
+            System.out.println("Courses can have up to 4 modules!");
+        } else if (isModuleExist(newModule)) {
+            System.out.println("The module is already added!");
+        }
+        else{
+            this.modules.add(newModule);
+            System.out.println("The module "+newModule.getTitile()+" was added successfuly.");
+        }
+    }
+
+    /**
+     * Checks wheter the given module already exists in the course`s modules
+     */
+    private boolean isModuleExist(Module newModule) {
+        for (Module module : this.modules) {
+            if (module.getTitile() == newModule.getTitile() && 
+                module.getModuleCode() == newModule.getModuleCode()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
