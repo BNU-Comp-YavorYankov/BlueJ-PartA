17a8939220903603cf16ef773f6321facfdfe5a3
yavaka <yavakasolutions@gmail.com>
2020-11-10 14:39:33 +0000

    App05: Add AddProductCommand class responsible for creating and adding a new product on stock.

---
+++ app05-stock/AddProductCommand.java
@@ -0,0 +1,54 @@
+/**
+ * The command for adding a new product on stock
+ * 
+ * @author Yavor Yankov
+ * @version 10/11/2020
+ */
+public class AddProductCommand implements Command 
+{
+    // The stock manager of the Stock Management Application
+    private StockManager manager;
+    // Use to get user input
+    private InputReader input;
+
+    /**
+     * The constructor of this command
+     * 
+     * @param manager The manager which is the reciever of the Command Design Pattern
+     */
+    public AddProductCommand(StockManager manager) {
+        this.manager = manager;
+        this.input = new InputReader();
+    }
+
+    /**
+     * Execute this command as create a new product and add it on stock.
+     * The method is overridden from Command interface.
+     */
+    @Override
+    public void execute() 
+    {
+        Product product = createProduct();
+
+        manager.addProduct(product);
+    }
+
+    /**
+     * Create a new product as use the user input for product id and name.
+     * 
+     * @return The newly created product.
+     */
+    private Product createProduct() 
+    {
+        System.out.println(" Add product:");
+
+        System.out.print("Id: ");
+        // Parse the user input from String to int
+        int productId = Integer.parseInt(input.getInput());
+        
+        System.out.print("Name: ");
+        String productName = this.input.getInput();
+
+        return new Product(productId,productName);
+    }
+}
