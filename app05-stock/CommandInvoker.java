e7d6e6aa6d92de2132c90e48cac3f8301862f1e5
yavaka <yavakasolutions@gmail.com>
2020-11-10 15:09:26 +0000

    App05: Add CommandInvoker class responsible for executing the command based on the user input.

---
+++ app05-stock/CommandInvoker.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+
+/**
+ * CommandInvoke is responsible to parse the user input and execute the proper command
+ * Command Design Pattern: The Invoker class
+ *  
+ * @author Yavor Yankov
+ * @version Change
+ */
+public class CommandInvoker {
+    /**
+     * All commands which can be executed 
+     * @key Keeps the name of the command
+     * @value Keeps the command class
+     */
+    private final HashMap<String,Command> commands;
+    /**
+     * The stock manager of this Stock Management Application.
+     * Command Design Pattern: this is the Reciever
+     **/
+    private StockManager manager;
+
+    /**
+     * The constructor of this command invoker class
+     */
+    public CommandInvoker() 
+    {
+        this.manager = new StockManager();
+        this.commands = new HashMap<String, Command>();
+  
+        seedAvailableCommands();
+    }
+
+    /**
+     * Execute the command based on the user input.
+     * 
+     * @param userInput The user input which will execute a command.
+     */
+    public void execute(String userInput) 
+    {
+        Command command = this.commands.get(userInput.toLowerCase());
+
+        if(command == null)
+        {
+            System.out.println(" Invalid command!");
+        }
+        else
+        {
+            command.execute();
+        }
+    }
+
+    /**
+     * Add all available commands in the commands collection
+     */
+    private void seedAvailableCommands() 
+    {
+        this.commands.put("add", new AddProductCommand(this.manager));
+    }
+}
