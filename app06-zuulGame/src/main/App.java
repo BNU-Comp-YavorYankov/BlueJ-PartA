5374317c45e477c3751e7afefb72a937e51fad5b
yavaka <yavakasolutions@gmail.com>
2021-01-18 16:07:02 +0000

    App06: Add use case diagrams images

--- app06-zuulGame/src/main/App.java
+++ app06-zuulGame/src/main/App.java
@@ -9,15 +9,13 @@ package main;
  */
 public class App
 {
-    private CommandInvoker invoker;
-    
     /**
      * This method creates and runs an instance of
      * the command invoker class
      */
     public static void main(String[] args)
     {
-        invoker = new CommandInvoker();
+        CommandInvoker invoker = new CommandInvoker();
         invoker.run();
     }
 }
